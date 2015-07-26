4813232e3b72db14a646225b3db68e6efcd128fa
AndyChen <shinningstar1001@gmail.com>
2015-07-27 04:22:58 +0800

    First Commit

---
+++ AutoKeyPresser/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace AutoKeyPresser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox_Customkeys = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_Interval = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox_Customkeys
+            // 
+            this.textBox_Customkeys.Location = new System.Drawing.Point(15, 63);
+            this.textBox_Customkeys.Name = "textBox_Customkeys";
+            this.textBox_Customkeys.Size = new System.Drawing.Size(246, 25);
+            this.textBox_Customkeys.TabIndex = 4;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 113);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(145, 15);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "send key interval : (Sec)";
+            // 
+            // textBox_Interval
+            // 
+            this.textBox_Interval.Location = new System.Drawing.Point(163, 110);
+            this.textBox_Interval.Name = "textBox_Interval";
+            this.textBox_Interval.Size = new System.Drawing.Size(100, 25);
+            this.textBox_Interval.TabIndex = 7;
+            this.textBox_Interval.Text = "1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 15);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "F1 : Collect(Auto E)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 36);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(139, 15);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "F2 : Auto Attack (Ctrl)";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(160, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(126, 15);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "F3 : Send key in text";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(160, 36);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 15);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "F4 : Stop";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(309, 147);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox_Interval);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_Customkeys);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_Customkeys;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_Interval;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ AutoKeyPresser/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace AutoKeyPresser
+{
+    public partial class Form1 : Form
+    {
+        // DLL libraries used to manage hotkeys
+        [DllImport("user32.dll")]
+        public static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vlc);
+        [DllImport("user32.dll")]
+        public static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        private string keySource;
+        public Form1()
+        {
+            InitializeComponent();
+            simpleRegistKey(Keys.F1);
+            simpleRegistKey(Keys.F2);
+            simpleRegistKey(Keys.F3);
+            simpleRegistKey(Keys.F4);
+        }
+
+        private void simpleRegistKey(Keys keys)
+        {
+            RegisterHotKey(this.Handle, (int)keys, 0, (int)keys);
+        }
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == 0x0312)
+            {
+                switch ((Keys)m.WParam)
+                {
+
+                    case Keys.F1:
+                        SendKey("{e}");
+                        break;
+                    case Keys.F2:
+                        SendKey("^",100);
+                        break;
+                    case Keys.F3:
+                        SendKey(textBox_Customkeys.Text);
+                        break;
+                    case Keys.F4:
+                        keySource = "";
+                        timer1.Enabled = false;
+                        break;
+                    //case Keys.F1:
+                    //    keySource = "";
+                    //    isRunning = !isRunning;
+                    //    timer1.Enabled = isRunning;
+                    //    break;
+                    //case Keys.E:
+                    //    if (!isRunning)
+                    //    {
+                    //        SendKeys.Send("{e}");
+                    //    }
+                    //   SendKey("{e}");
+                    //    break;
+                    //case Keys.F5:
+                    //    if (!isRunning)
+                    //    {
+                    //        SendKeys.Send("^");
+                    //    }
+                    //    SendKey("^", 100);
+                    //    break;
+                    //case Keys.F2:
+                    //    SendKey(textBox_Customkeys.Text, 100);
+                    //    break;
+                }
+            }
+            base.WndProc(ref m);
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (keySource != "")
+            {
+                SendKeys.Send(keySource);
+               // SendKeys.Send("{Enter}");
+            }
+        }
+        private void SendKey(string key, int speed=1000)
+        {
+            keySource = key;
+            timer1.Enabled = true;
+            timer1.Interval = speed * Convert.ToInt32(textBox_Interval.Text);
+        }
+    }
+}
